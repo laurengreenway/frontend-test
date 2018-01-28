a7eedf73aa9be46edb938bff60337aad7fb1b916
Lauren Greenway <lagreenway@gmail.com>
2018-01-28 14:25:57 -0500

    seperate file for app data

---
+++ app/src/data.js
@@ -0,0 +1,36 @@
+import bcthmb from './assets/bc-thmb.png'
+import logothmb from './assets/logo-thmb.png'
+import shirtthmb from './assets/t-shirt-thmb.png'
+import socialthmb from './assets/social-media.png'
+
+import bclg from './assets/bc-lg.png'
+import logolg from './assets/logo-lg.png'
+import shirtlg from './assets/shirt-lg.png'
+import sociallg from './assets/sm-lg.png'
+
+export const brandAssets = [
+  {
+    thumbnail: logothmb,
+    large: logolg,
+    alt: 'Logo',
+    id: 0,
+  },
+  {
+    thumbnail: shirtthmb,
+    large: shirtlg,
+    alt: 'T-Shirts',
+    id: 1,
+  },
+  {
+    thumbnail: bcthmb,
+    large: bclg,
+    alt: 'Business Cards',
+    id: 2,
+  },
+  {
+    thumbnail: socialthmb,
+    large: sociallg,
+    alt: 'Social Media',
+    id: 3,
+  },
+]
